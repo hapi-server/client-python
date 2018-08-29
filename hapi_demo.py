17cea432bcede7172f532adee45a3fb6dc22ba9b
Robert Weigel <rweigel@gmu.edu>
2018-08-29 12:34:33 -0400

    CDAWeb demo

--- hapi_demo.py
+++ hapi_demo.py
@@ -3,97 +3,79 @@ from hapiplot import hapiplot
 import matplotlib.pyplot as plt 
 import json
 
+# In IPython, enter %matplotlib qt on command line to open plots in 
+# new window. Enter %matplotlib inline to revert.
+
 plt.close("all")
 
-# Specify format to use (for testing only; by default it will check 
-# response from /capabilities and use binary if avaialble).
-format = 'csv'
+#############################################################################
+#%% CDAWeb example
+server     = 'https://cdaweb.gsfc.nasa.gov/hapi'
+dataset    = 'AC_H0_MFI'
+start      = '2001-01-01T05:00:00'
+stop       = '2001-01-01T06:00:00'
+parameters = 'Magnitude,BGSEc'
+opts       = {'format': 'binary', 'logging': True, 'use_cache': False}
+data,meta = hapi(server, dataset, parameters, start, stop, **opts)
+hapiplot(data,meta,logging=True)
 
+#############################################################################
+#%% Test data 
 server     = 'http://mag.gmu.edu/TestData/hapi';
-#server     = 'http://localhost:8999/hapi'
+server     = 'http://localhost:8999/TestData/hapi'
 dataset    = 'dataset1'
 start      = '1970-01-01'
 stop       = '1970-01-01T00:00:10'
-opts       = {'format': format, 'logging': True, 'use_cache': False}
+opts       = {'logging': True, 'use_cache': False}
+popts      = {'logging': True}
 
 #############################################################################
-# Scalar
+#%% Scalar
 parameters = 'scalar'
-
 data,meta = hapi(server, dataset, parameters, start, stop, **opts)
-print(data['scalar'].dtype)
-print(data['scalar'].shape)
-
-hapiplot(data,meta)
+hapiplot(data,meta,**popts)
 #############################################################################
 
 #############################################################################
-# Scalar with integer type
+#%% Scalar with integer type
 parameters = 'scalar'
-
 data,meta = hapi(server, dataset, parameters, start, stop, **opts)
-print(data['scalar'].dtype)
-print(data['scalar'].shape)
-
-hapiplot(data,meta)
+hapiplot(data,meta,**popts)
 #############################################################################
 
 #############################################################################
-# Scalar with string type
+#%% Scalar with string type
 parameters = 'scalarstr'
-
 data,meta = hapi(server, dataset, parameters, start, stop, **opts)
-print(data['scalarstr'].dtype)
-print(data['scalarstr'].shape)
-
-hapiplot(data,meta)
+hapiplot(data,meta,**popts)
 #############################################################################
 
 #############################################################################
-# Scalar with isotime type
+#%% Scalar with isotime type
 parameters = 'scalariso'
-
 data,meta = hapi(server, dataset, parameters, start, stop, **opts)
-print(data['scalariso'].dtype)
-print(data['scalariso'].shape)
-
 #hapiplot(data,meta)
 #############################################################################
 
 #############################################################################
-# Vector
+#%% Vector
 parameters = 'vector'
-
 data,meta = hapi(server, dataset, parameters, start, stop, **opts)
-print(data['vector'].dtype)
-print(data['vector'].shape)
-
-hapiplot(data,meta)
+hapiplot(data,meta,**popts)
 #############################################################################
 
 #############################################################################
-# Two parameters
+#%% Two parameters
 parameters = 'scalar,vector'
-
 data,meta = hapi(server, dataset, parameters, start, stop, **opts)
-print(data['scalar'].dtype)
-print(data['scalar'].shape)
-print(data['vector'].dtype)
-print(data['vector'].shape)
-
-#hapiplot(data,meta)
+hapiplot(data,meta,**popts)
 #############################################################################
 
 #############################################################################
-# Metadata request examples
-sn = 2 # Server number in servers.txt
+#%% Metadata request examples
+sn = 0 # Server number in servers.txt
 dn = 0 # Dataset number from server sn
-pn = 1 # Parameter number in Dataset dn
-
-# Default parameters
-opts = {'update_script': False,'logging': False,
-        'cache_mlbin': True,'cache_hapi': True,
-        'use_cache': True,'format': 'fbinary'}
+pn = 0 # Parameter number in Dataset dn
 
 # List servers to console
 hapi(logging=True)
@@ -101,7 +83,7 @@ hapi(logging=True)
 # hapi(**opts)
 
 # Get server list
-Servers = hapi();
+Servers = hapi()
 # or
 # Servers = hapi(**opts)
 print("Server list")
