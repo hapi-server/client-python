6df4b8a2d5d471834d16f881b66dc08f0eb060bb
Robert Weigel <rweigel@gmu.edu>
2018-08-29 12:34:59 -0400

    save png, use datetick

--- hapiplot.py
+++ hapiplot.py
@@ -1,16 +1,34 @@
 import matplotlib.pyplot as plt 
 import pandas
 import warnings
-#import matplotlib.dates as mdates
+import numpy as np
+import re
+import sys
 
-def hapiplot(data,meta):
+sys.path.insert(0, './misc')
+from datetick import datetick
+
+def printf(format, *args): sys.stdout.write(format % args)
+
+def hapiplot(data,meta,**kwargs):
+
+    # Default options
+    DOPTS = {}
+    DOPTS.update({'logging': False})
+
+    # Override defaults
+    for key, value in kwargs.items():
+        if key in DOPTS:
+            DOPTS[key] = value
+        else:
+            print('Warning: Keyword option "%s" is not valid.' % key)
 
     fignums = plt.get_fignums()
     if len(fignums) == 0:
         fignums = [0]
     lastfn = fignums[-1]
     
-    Time = pandas.to_datetime(data['Time'],infer_datetime_format=True)
+    Time = pandas.to_datetime(data['Time'].astype('U'),infer_datetime_format=True)
     
     for i in range(1,len(meta["parameters"])):
         if meta["parameters"][i]["type"] != "double" and meta["parameters"][i]["type"] != "integer":
@@ -18,10 +36,25 @@ def hapiplot(data,meta):
             continue
 
         name = meta["parameters"][i]["name"]
+        y = np.asarray(data[name])
+    
+        if meta["parameters"][i]["fill"]:
+            if meta["parameters"][i]["fill"] == 'nan':
+                yfill = np.nan
+            else:
+                yfill = float(meta["parameters"][i]["fill"])  
+                #if isnan(yfill),yfill = 'null';,end
+        else:
+            yfill = 'null'
 
+        if yfill != 'null':
+            # Replace fills with NaN for plotting
+            # (so gaps shown in lines for time series)
+            y[y == yfill] = np.nan;    
+        
         plt.figure(lastfn + i)
         plt.clf()
-        plt.plot(Time,data[name])
+        plt.plot(Time,y)
         plt.gcf().canvas.set_window_title(meta["x_server"] + " | " + meta["x_dataset"] + " | " + name)
 
         yl = meta["parameters"][i]["name"] + " [" + meta["parameters"][i]["units"] + "]"
@@ -29,4 +62,12 @@ def hapiplot(data,meta):
         plt.title(meta["x_server"] + "/info?id=" + meta["x_dataset"] + "&parameters=" + name, fontsize=10)
 
         plt.grid()
+        datetick('x')
+        
+        fnamepng = re.sub('\.csv|\.bin','.png',meta['x_dataFile'])
+        if DOPTS['logging']: printf('Writing %s ... ', fnamepng)
+        plt.figure(lastfn + i).savefig(fnamepng,dpi=300) 
+        if DOPTS['logging']: printf('Done.\n')
+
+        # Important: This must go after savefig or else the png is blank.
         plt.show()
