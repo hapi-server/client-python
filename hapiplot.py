c85cdea8b9ecb1870ffff1678a7fd4274f37cc51
Robert Weigel <rweigel@gmu.edu>
2018-09-07 11:10:10 -0400

    Move time fn to end

--- hapiplot.py
+++ hapiplot.py
@@ -20,6 +20,94 @@ def urlretrieve(url,fname):
 
 def printf(format, *args): sys.stdout.write(format % args)
 
+def hapiplot(data,meta,**kwargs):
+
+    #%% Download and import datetick.py 
+    # TODO: Incorporate datetick.py into hapiplot.py
+    url = 'https://github.com/hapi-server/client-python/raw/master/misc/datetick.py'
+    print(url)
+    if os.path.isfile('datetick.py') == False and os.path.isfile('misc/datetick.py') == False:
+        urlretrieve(url,'datetick.py')
+    if os.path.isfile('misc/datetick.py') == True:
+        sys.path.insert(0, './misc')
+
+    from datetick import datetick
+    
+    # Default options
+    DOPTS = {}
+    DOPTS.update({'logging': False})
+
+    # Override defaults
+    for key, value in kwargs.items():
+        if key in DOPTS:
+            DOPTS[key] = value
+        else:
+            print('Warning: Keyword option "%s" is not valid.' % key)
+
+    fignums = plt.get_fignums()
+    if len(fignums) == 0:
+        fignums = [0]
+    lastfn = fignums[-1]
+    
+    try:
+        # Will fail if no pandas, if YYY-DOY format and other valid ISO 8601
+        # dates such as 2001-01-01T00:00:03.Z
+        Time = pandas.to_datetime(data['Time'].astype('U'),infer_datetime_format=True)
+    except:
+        # Slow and manual parsing.
+        Time = hapitime2datetime(data['Time'].astype('U'))
+        
+    # In the following, the x parameter is a datetime object.
+    # If the x parameter is a number, would need to use plt.plot_date()
+    # and much of the code for datetick.py would need to be modified.
+    for i in range(1,len(meta["parameters"])):
+        if meta["parameters"][i]["type"] != "double" and meta["parameters"][i]["type"] != "integer":
+            warnings.warn("Plots for types double and integer only implemented.")
+            continue
+
+        name = meta["parameters"][i]["name"]
+        y = np.asarray(data[name])
+    
+        if meta["parameters"][i]["fill"]:
+            if meta["parameters"][i]["fill"] == 'nan':
+                yfill = np.nan
+            else:
+                yfill = float(meta["parameters"][i]["fill"])  
+                #if isnan(yfill),yfill = 'null';,end
+        else:
+            yfill = 'null'
+
+        if yfill != 'null':
+            # Replace fills with NaN for plotting
+            # (so gaps shown in lines for time series)
+            y[y == yfill] = np.nan;    
+
+        if y.shape[0] < 11:
+            props = {'linestyle': 'none','marker': '.','markersize': 16}
+        elif y.shape[0] < 101:
+            props = {'lineStyle': '-','linewidth': 2, 'marker': '.', 'markersize': 8}
+        else:
+            props = {}
+        plt.figure(lastfn + i)
+        plt.clf()
+        plt.plot(Time,y,**props)
+        plt.gcf().canvas.set_window_title(meta["x_server"] + " | " + meta["x_dataset"] + " | " + name)
+
+        yl = meta["parameters"][i]["name"] + " [" + meta["parameters"][i]["units"] + "]"
+        plt.ylabel(yl)
+        plt.title(meta["x_server"] + "/info?id=" + meta["x_dataset"] + "&parameters=" + name + "\n", fontsize=10)
+        plt.grid()
+        datetick('x')
+        #import pdb; pdb.set_trace()
+        
+        fnamepng = re.sub('\.csv|\.bin','.png',meta['x_dataFile'])
+        if DOPTS['logging']: printf('Writing %s ... ', fnamepng)
+        plt.figure(lastfn + i).savefig(fnamepng,dpi=300) 
+        if DOPTS['logging']: printf('Done.\n')
+
+        # Important: This must go after savefig or else the png is blank.
+        plt.show()
+
 def hapitime2datetime(Time):
 
     import re
@@ -82,82 +170,3 @@ def hapitime2datetime(Time):
     toc = time.time()-tic
     #print('%.4fs' % toc)
     return dateTimeString
-
-def hapiplot(data,meta,**kwargs):
-
-    #%% Download and import datetick.py 
-    # TODO: Incorporate datetick.py into hapiplot.py
-    url = 'https://github.com/hapi-server/client-python/raw/master/misc/datetick.py'
-    print(url)
-    if os.path.isfile('datetick.py') == False and os.path.isfile('misc/datetick.py') == False:
-        urlretrieve(url,'datetick.py')
-    if os.path.isfile('misc/datetick.py') == True:
-        sys.path.insert(0, './misc')
-
-    from datetick import datetick
-    
-    # Default options
-    DOPTS = {}
-    DOPTS.update({'logging': False})
-
-    # Override defaults
-    for key, value in kwargs.items():
-        if key in DOPTS:
-            DOPTS[key] = value
-        else:
-            print('Warning: Keyword option "%s" is not valid.' % key)
-
-    fignums = plt.get_fignums()
-    if len(fignums) == 0:
-        fignums = [0]
-    lastfn = fignums[-1]
-    
-    try:
-        Time = pandas.to_datetime(data['Time'].astype('U'),infer_datetime_format=True)
-    except:
-        Time = hapitime2datetime(data['Time'].astype('U'))
-        
-    # In the following, the x parameter is a datetime object.
-    # If the x parameter is a number, would need to use plt.plot_date()
-    # and much of the code for datetick.py would need to be modified.
-    for i in range(1,len(meta["parameters"])):
-        if meta["parameters"][i]["type"] != "double" and meta["parameters"][i]["type"] != "integer":
-            warnings.warn("Plots for types double and integer only implemented.")
-            continue
-
-        name = meta["parameters"][i]["name"]
-        y = np.asarray(data[name])
-    
-        if meta["parameters"][i]["fill"]:
-            if meta["parameters"][i]["fill"] == 'nan':
-                yfill = np.nan
-            else:
-                yfill = float(meta["parameters"][i]["fill"])  
-                #if isnan(yfill),yfill = 'null';,end
-        else:
-            yfill = 'null'
-
-        if yfill != 'null':
-            # Replace fills with NaN for plotting
-            # (so gaps shown in lines for time series)
-            y[y == yfill] = np.nan;    
-        
-        plt.figure(lastfn + i)
-        plt.clf()
-        plt.plot(Time,y)
-        plt.gcf().canvas.set_window_title(meta["x_server"] + " | " + meta["x_dataset"] + " | " + name)
-
-        yl = meta["parameters"][i]["name"] + " [" + meta["parameters"][i]["units"] + "]"
-        plt.ylabel(yl)
-        plt.title(meta["x_server"] + "/info?id=" + meta["x_dataset"] + "&parameters=" + name + "\n", fontsize=10)
-        plt.grid()
-        datetick('x')
-        #import pdb; pdb.set_trace()
-        
-        fnamepng = re.sub('\.csv|\.bin','.png',meta['x_dataFile'])
-        if DOPTS['logging']: printf('Writing %s ... ', fnamepng)
-        plt.figure(lastfn + i).savefig(fnamepng,dpi=300) 
-        if DOPTS['logging']: printf('Done.\n')
-
-        # Important: This must go after savefig or else the png is blank.
-        plt.show()
