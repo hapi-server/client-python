dc1162f65322f91d28d2f7d8a8610501959ee7dc
Robert Weigel <rweigel@gmu.edu>
2018-09-17 18:12:14 -0400

    hardwire version

--- setup.py
+++ setup.py
@@ -1,16 +1,10 @@
-from distutils.core import setup
+#from distutils.core import setup
+from setuptools import setup
 import sys
-
-Version = '0.0.4'
-
-if sys.version_info[0] >= 3:
-    install_requires = ["numpy>=1.14.3","pandas==0.23.0","matplotlib>=2.2.2"]
-else:
-    install_requires = ["numpy>=1.14.3","pandas==0.23.0","matplotlib>=2.2.2"]
 
 setup(
     name='hapiclient',
-    version=Version,
+    version='0.0.34',
     author='Bob Weigel',
     author_email='rweigel@gmu.edu',
     packages=['hapiclient'],
@@ -18,5 +12,5 @@ setup(
     license='LICENSE.txt',
     description='Heliophysics API',
     long_description=open('README.txt').read(),
-    install_requires=install_requires
+    install_requires=["numpy>=1.14.3","pandas==0.23.0","matplotlib>=2.2.2"]
 )
