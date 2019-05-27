de3743e5250808a0c64344ae8b3457ccef1e3a47
Robert Weigel <rweigel@gmu.edu>
2019-05-27 16:12:45 -0500

    Remove unused files

--- misc/version.py
+++ misc/version.py
@@ -1,14 +1,18 @@
-import re
-import sys
+"""
+Reads CHANGES.txt and finds last version. Updates version strings in Makefile,
+setup.py, hapi.py, and hapiclient.py to match this version.
 
-# TODO: Find better way to manage this.
+TODO: Find better way to manage this.
+"""
+
+import re
 
 # Get last version in CHANGES.txt
 print("Finding version information from CHANGES.txt")
 fin = open("CHANGES.txt")
 version = '0.0.0'
 for line in fin:
-	(repl,n) = re.subn(r"^v(.*):.*", r"\1", line)
+	(repl, n) = re.subn(r"^v(.*):.*", r"\1", line)
 	if n > 0:
 		version = repl
 fin.close()
@@ -24,8 +28,8 @@ for lineo in fin:
 fin.close()
 assert updated is False, "Problem updating version in Makefile."
 with open("Makefile.tmp", "w") as fout:
-    fout.write(lines)
-fout.close()    
+	fout.write(lines)
+fout.close()
 print("Wrote Makefile.tmp")
 
 lines = ''
@@ -37,14 +41,13 @@ for lineo in fin:
 fin.close()
 assert updated is False, "Problem updating version in setup.py."
 with open("setup.py.tmp", "w") as fout:
-    fout.write(lines)
-fout.close()    
+	fout.write(lines)
+fout.close()
 print("Wrote setup.py.tmp")
 
-for fname in ("hapiclient/hapi.py","hapiclient/hapiplot.py"):
+for fname in ("hapiclient/hapi.py", "hapiclient/hapiplot.py"):
 	lines = ''
 	fin = open(fname)
-	#sys.stdout.write("Updating version in " + fname + ".")
 	for lineo in fin:
 		line1 = re.sub(r"__version__ = '(.*)'", r"__version__ = '" + version + "'", lineo)
 		updated1 = lineo != line1
@@ -52,8 +55,9 @@ for fname in ("hapiclient/hapi.py","hapiclient/hapiplot.py"):
 		updated2 = lineo != line2
 		lines = lines + line2
 	fin.close()
-	assert updated1 is False or updated2 is False, "Problem updating version in " + fname + "."
+	assert updated1 is False or updated2 is False, \
+		"Problem updating version in " + fname + "."
 	with open(fname + ".tmp", "w") as fout:
-	    fout.write(lines)
+		fout.write(lines)
 	fout.close()    
 	print("Wrote " + fname + ".tmp")
