314c3263f8a01cc011abd3241b3d079bea8ab517
weigel <rweigel@gmu.edu>
2020-07-28 17:44:57 -0400

    python 2.7 fix

--- setup.py
+++ setup.py
@@ -3,14 +3,16 @@ import sys
 
 install_requires = ["isodate>=0.6.0"]
 
-# Python 3.6 and Matplotlib 3.3 on OS-X 10.15.4 give error when using
-# rc = matplotlib.style: 
-# AttributeError: module 'matplotlib' has no attribute 'style'
+print(sys.version_info)
 if sys.version_info >= (3, 6):
+    # Python 3.6 and Matplotlib 3.3 on OS-X 10.15.4 give error when using
+    # rc = matplotlib.style: 
+    # AttributeError: module 'matplotlib' has no attribute 'style'
+    # TODO: Is this needed for python 3.7+?
     install_requires.append('matplotlib>=2.2.2,<3.3')
     install_requires.append('pandas>=0.23')
     install_requires.append('numpy>=1.14.3')
-elif sys.version_info < (3, 6):
+elif sys.version_info < (3, 6) and sys.version_info > (3, ):
     # Beginning with Matplotlib 3.1, Python 3.6 or above is required.
     install_requires.append('matplotlib>=2.2.2,<3.0')
     install_requires.append("pandas<1.0")
@@ -18,10 +20,13 @@ elif sys.version_info < (3, 6):
     install_requires.append("kiwisolver<1.2")
 else:
     install_requires.append('matplotlib==2.*')
-    install_requires.append("pandas>=0.23,<1.0")
+    install_requires.append("pandas>=0.23,<0.25")
     install_requires.append("numpy<1.17")
+    install_requires.append("pyparsing<3")
+    install_requires.append("zipp<3")
 
 if sys.argv[1] == 'develop':
+    install_requires.append("deepdiff<3.3.0")
     if sys.version_info < (3, 6):
         install_requires.append("pytest<5.0.0")
     else:
@@ -29,8 +34,6 @@ if sys.argv[1] == 'develop':
         # https://docs.pytest.org/en/stable/py27-py34-deprecation.html
         # Perhaps old version of pip causes this?
         install_requires.append("pytest")
-        
-    install_requires.append("deepdiff<3.3.0")
 
 # version is modified by misc/version.py (executed from Makefile). Do not edit.
 setup(
