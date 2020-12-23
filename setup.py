96bf7730cff5e27b18ad120d03313bd5d18c120f
Bob Weigel <rweigel@gmu.edu>
2020-12-23 18:33:14 -0500

    Version increment (to v0.1.9)

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ if sys.argv[1] == 'develop':
 # version is modified by misc/version.py (executed from Makefile). Do not edit.
 setup(
     name='hapiclient',
-    version='0.1.8',
+    version='0.1.9',
     author='Bob Weigel',
     author_email='rweigel@gmu.edu',
     packages=find_packages(),
