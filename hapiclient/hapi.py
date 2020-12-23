a274c1bc648fbee3a6d484bcbbc21d1085327b08
Bob Weigel <rweigel@gmu.edu>
2020-12-23 14:02:45 -0500

    Last 0.1.7 commit

--- hapiclient/hapi.py
+++ hapiclient/hapi.py
@@ -144,7 +144,7 @@ def hapi(*args, **kwargs):
     For additional documentation and demonstration, see
     https://github.com/hapi-server/client-python-notebooks/blob/master/hapi_demo.ipynb
 
-    Version: 0.1.6
+    Version: 0.1.7
 
     Parameters
     ----------
