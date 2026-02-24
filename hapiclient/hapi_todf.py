adf9479c343131a3abe4f1d36daa94d9d1e6d5d3
Lucy Wilkerson <lucy.a.wilkerson@gmail.com>
2026-02-23 19:28:39 -0500

    Update hapi_todf.py

--- hapiclient/hapi_todf.py
+++ hapiclient/hapi_todf.py
@@ -34,30 +34,47 @@ def hapidata2dataframe(meta, data,
     import pandas
     import numpy as np
 
+    import warnings
+    warnings.filterwarnings('ignore', category=UserWarning) # suppress UserWarning from hapitime2datetime:
+            # UserWarning: The argument 'infer_datetime_format' is deprecated and will be removed in a future version. 
+            # A strict version of it is now the default, see https://pandas.pydata.org/pdeps/0004-consistent-to-datetime-parsing.html. 
+            # You can safely remove this argument.
+
     #pandas.set_option('display.max_columns', None) # display all columns when printing dataframe, for testing
 
     dfs = []
+
+    time_name = meta['parameters'][0]['name']
+    if parameters_include is not None: # only include parameters in parameters_include
+        for param in parameters_include:
+            if param not in [p['name'] for p in meta['parameters']]: # if parameter in parameters_include but not in meta['parameters'], raise error
+                raise ValueError(f"Parameter {param} in parameters_include is not in meta['parameters']")
+        if time_name not in parameters_include: 
+            parameters_include = parameters_include.copy()
+            parameters_include.append(time_name) # always include time parameter
+    if parameters_exclude is not None: # exclude parameters in parameters_exclude
+        for param in parameters_exclude:
+            if param not in [p['name'] for p in meta['parameters']]: # if parameter in parameters_exclude but not in meta['parameters'], raise error
+                raise ValueError(f"Parameter {param} in parameters_exclude is not in meta['parameters']")
+        if time_name in parameters_exclude:
+            raise ValueError(f"Time parameter {time_name} cannot be excluded in parameters_exclude")
+
+    if name_map is not None: # if name in name_map but not in parameter names used, raise error
+        for name in name_map: 
+            if parameters_include is not None and name not in parameters_include:
+                raise ValueError(f"Name {name} in name_map does not match any parameter names in parameters_include")
+            elif name not in [p['name'] for p in meta['parameters']]:
+                raise ValueError(f"Name {name} in name_map does not match any parameter names in meta['parameters']")
+
     for param in meta['parameters']: 
         param_name = param['name']
-        time_name = meta['parameters'][0]['name']
         if parameters_include is not None: # only include parameters in parameters_include
-            for param in parameters_include:
-                if param not in [p['name'] for p in meta['parameters']]: # if parameter in parameters_include but not in meta['parameters'], raise error
-                    raise ValueError(f"Parameter {param} in parameters_include is not in meta['parameters']")
-            if time_name not in parameters_include: 
-                parameters_include = parameters_include.copy()
-                parameters_include.append(time_name) # always include time parameter
             if param_name not in parameters_include:
                 continue
         if parameters_exclude is not None: # exclude parameters in parameters_exclude
             if param_name in parameters_exclude:
                 continue
         if param_name == time_name: # always convert Time to datetime
-            import warnings
-            warnings.filterwarnings('ignore', category=UserWarning) # suppress UserWarning from hapitime2datetime:
-            # UserWarning: The argument 'infer_datetime_format' is deprecated and will be removed in a future version. 
-            # A strict version of it is now the default, see https://pandas.pydata.org/pdeps/0004-consistent-to-datetime-parsing.html. 
-            # You can safely remove this argument.
             df_temp = pandas.DataFrame({param_name: hapitime2datetime(data[param_name])})
             dfs.append(df_temp)
         else:
@@ -67,9 +84,10 @@ def hapidata2dataframe(meta, data,
                 if param['type'] == 'isotime': 
                     param_data = hapitime2datetime(param_data) 
                     if fill == "0000-00-00T00:00:00Z": # to avoid error converting to datetime
-                        fill = "0001-01-01T00:00:00Z"
+                        fill = hapitime2datetime("0001-01-01T00:00:00Z")
                         param_data = np.where(param_data == hapitime2datetime("0000-00-00T00:00:00Z"), fill, param_data) # replace fill value in data
-                    fill = hapitime2datetime(fill)
+                    else:
+                        fill = hapitime2datetime(fill)
                     param_data = np.where(param_data == fill, np.datetime64('NaT', 'us'), param_data) # convert to datetime and fill with NaT
                 elif param['type'] == 'integer':
                     param_data = np.where(param_data == int(fill), np.nan, param_data)
@@ -87,18 +105,14 @@ def hapidata2dataframe(meta, data,
                 ndim = param_data.ndim
             # handle column names based on number of dimensions
             ndim = param_data.ndim
-            if ndim <= 3:
-                if ndim == 1:
-                    col_names = [param_name]
-                else:
-                    # use MultiIndex for 2D and 3D parameters 
-                    # in theory, could be used for higher dimensions too, but would be ugly
-                    index_arrays = [[param_name]] + [list(map(str, range(param_data.shape[i]))) for i in range(1, ndim)]
-                    col_names = pandas.MultiIndex.from_product(index_arrays)
-                # define temp dataframe
-                df_temp = pandas.DataFrame(param_data.reshape(param_data.shape[0], -1), columns=col_names)
+            if ndim == 1:
+                col_names = [param_name]
             else:
-                raise ValueError(f"Parameter {param_name} has unsupported number of dimensions: {param_data.ndim}")
+                # use MultiIndex for >1D parameters
+                index_arrays = [[param_name]] + [list(map(str, range(param_data.shape[i]))) for i in range(1, ndim)]
+                col_names = pandas.MultiIndex.from_product(index_arrays)
+            # define temp dataframe
+            df_temp = pandas.DataFrame(param_data.reshape(param_data.shape[0], -1), columns=col_names)
             # add dataframe to list
             dfs.append(df_temp)
     # concatenate all dataframes
@@ -106,9 +120,6 @@ def hapidata2dataframe(meta, data,
     # set index to Time
     df.set_index(time_name, inplace=True)
     if name_map is not None: # rename columns using name_map
-        for name in name_map: # if name in name_map but not in df.columns, raise error
-            if name not in df.columns.get_level_values(0):
-                raise ValueError(f"Name {name} does not match any parameter names in df.columns: {df.columns.get_level_values(0)}")
         # for MultiIndex columns, only rename the first level (parameter name), not the second level (column number)
         df.rename(columns=lambda x: (name_map.get(x[0], x[0]),) + x[1:] if isinstance(x, tuple) else name_map.get(x, x), inplace=True)
         if time_name in name_map:
